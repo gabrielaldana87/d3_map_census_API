f254c4dbc55afdc445a99e094c451c02e1fd0a93
gabrielaldana87 <gabrielaldana87@gmail.com>
2015-02-08 22:09:45 -0500

    renders an income map of the NY metropolitan area as well as server route to handle API request

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/public'));
 fs.readFile("./key.txt",function(e,data)
 {
   var key = data.toString();
-  // var url = "http://api.census.gov/data/2012/acs5/profile?get=DP03_0062E&for=county:*"+key;
+  var url = "http://api.census.gov/data/2012/acs5/profile?get=DP03_0062E&for=county:*"+key;
 
 app.get('/', function(req, res)
 {
@@ -53,6 +53,48 @@ app.get('/', function(req, res)
   });
   res.sendFile(__dirname + '/public/map.html');
 });
+app.get('/test', function(req,res)
+{
+  var array=[];
+  var url = "http://api.census.gov/data/2013/acs5/profile?get=DP03_0062E&for=zip+code+tabulation+area:*"+key;
+  console.log(url);
+  request(url, function(error, response, body)
+  {
+    if(!error && response.statusCode === 200)
+      {
+        var data = JSON.parse(body);
+
+        data.forEach(function(zip)
+        {
+          var datum =
+          {
+            id: zip[1],
+            rate: parseFloat(zip[0])
+          }
+          array.push(datum);
+        });
+        // var county=data[1][1]+data[1][2];
+
+        fs.writeFile("./public/zip_rate.json",JSON.stringify(array),function(e)
+        {
+          console.log("done!");
+        })
+
+        var lowest = Number.POSITIVE_INFINITY;
+        var highest = Number.NEGATIVE_INFINITY;
+        var tmp;
+
+        for(var i=array.length-1;i>=1;i--)
+          {
+            tmp = array[i].rate;
+            if(tmp < lowest) lowest=tmp;
+            if(tmp > highest) highest =tmp;
+          }
+          console.log("highest: "+highest,"lowest: "+lowest);
+        }
+      });
+      res.sendFile(__dirname + '/public/nyc_map.html');
+    })
 
 app.get('/:indicator',function(req, res)
 {
@@ -106,13 +148,15 @@ app.get('/:indicator',function(req, res)
     };
 })
 
-});
+
 
 app.get('/nums', function(req, res)
 {
   res.json(max_min);
 })
 
+
+});
 var server = app.listen(3000,function()
 {
   console.log("listening on port 3000")
