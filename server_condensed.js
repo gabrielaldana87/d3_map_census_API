45e7df74618102e4b823ecfb11309c09d46f556b
gabrielaldana87 <gabrielaldana87@gmail.com>
2015-03-14 15:05:56 -0400

    testing

--- server_condensed.js
+++ server_condensed.js
@@ -9,8 +9,9 @@ var db = new sqlite3.Database("mapping.db");
 
 var app = express();
 app.use(cors());
-//
-// var max_min = [];
+
+var array = [];
+var array2 = [];
 
 app.use(express.static(__dirname + '/public'));
 
@@ -32,6 +33,121 @@ db.all("SELECT * FROM variables", function(err, rows)
   });
 });
 
+app.get('/zip', function(req,res)
+{
+  res.sendFile(__dirname + '/public/nyc_map.html');
+});
+
+
+app.get('/CREATE/:indicator',function(req, res)
+  {
+  if(req.params.indicator!=='favicon.ico')
+    {
+    var url  = "http://api.census.gov/data/2012/acs5/profile?get="+req.params.indicator+"&for=county:*"+key;
+    var url3 = "http://api.census.gov/data/2013/acs5/profile/variables/"+req.params.indicator+".json"
+    var url2 = "http://api.census.gov/data/2013/acs5/profile?get="+req.params.indicator+"&for=zip+code+tabulation+area:*"+key;
+
+    fs.readFile("./public/variables.json",function(e,data)
+    {
+      var read = JSON.parse(data);
+
+for(i=0;i<read.length;i++)
+
+        {if(req.params.indicator===read[i].id)
+        { return written()}
+
+        else
+        {
+          return request_var(read);
+        };
+        };
+
+
+
+    });
+
+    var request_var = function(read)
+    {
+      request(url3, function(error, response, body)
+      {
+        if(!error && response.statusCode === 200)
+          {
+            var data = JSON.parse(body);
+            console.log(data.name);
+            var obj =
+            {
+              id: data.name,
+              concept: data.concept,
+              label: data.label
+            };
+            read.push(obj);
+
+            fs.writeFile("./public/variables.json", JSON.stringify(read),function(e)
+            {
+              console.log("Variable added!");
+            });
+          }
+      });
+
+      request(url, function(error, response, body)
+        {
+        if(!error && response.statusCode === 200)
+          {
+          var data = JSON.parse(body);
+          data.forEach(function(fips)
+            {
+            var obj =
+              {
+              id: fips[1]+fips[2],
+              rate: parseFloat(fips[0])
+              };
+            array.push(obj);
+            });
+            fs.writeFile("./public/"+req.params.indicator+".json", JSON.stringify(array),function(e)
+            {
+              console.log("County variable written!");
+            });
+          }
+        });
+
+      request(url2, function(error, response, body)
+      {
+        if(!error && response.statusCode === 200)
+          {
+            var data = JSON.parse(body);
+
+            data.forEach(function(zip)
+            {
+
+              if(zip[1].substring(0,1)==='1')
+              {
+                var datum =
+                {
+                id: zip[1],
+                rate: parseFloat(zip[0])
+                }
+                array2.push(datum);
+              };
+            });
+
+            fs.writeFile("./public/zip_var/"+req.params.indicator+".json",JSON.stringify(array2),function(e)
+            {
+              console.log("Zip Code Rate done!");
+            });
+
+          }
+        });
+    };
+
+    var written = function()
+    {
+      console.log("this variable is already written");
+    }
+
+    };
+});
+
+
 app.get('/:indicator',function(req, res)
   {
   if(req.params.indicator!=='favicon.ico')
@@ -43,73 +159,45 @@ app.get('/:indicator',function(req, res)
             if(err) {throw err;}
             res.json(row);
             });
-          // }
-      // });
 
-    // request(url2, function(error, response, body)
-    // {
-    //   if(!error && response.statusCode === 200)
-    //     {
-    //       var data = JSON.parse(body);
-    //
-    //       data.forEach(function(zip)
-    //       {
-    //         var datum =
-    //         {
-    //           id: zip[1],
-    //           rate: parseFloat(zip[0])
-    //         }
-    //         array2.push(datum);
-    //       });
-    //
-    //       fs.writeFile("./public/zip_rate.json",JSON.stringify(array2),function(e)
-    //       {
-    //         console.log("Zip Code Rate done!");
-    //       })
-    //
-    //       var lowest = Number.POSITIVE_INFINITY;
-    //       var highest = Number.NEGATIVE_INFINITY;
-    //       var tmp;
-    //
-    //       for(var i=array.length-1;i>=1;i--)
-    //         {
-    //           tmp = array2[i].rate;
-    //           if(tmp < lowest) lowest=tmp;
-    //           if(tmp > highest) highest =tmp;
-    //         }
-    //         console.log("highest: "+highest,"lowest: "+lowest);
-    //       }
-    //   });
+    var url2 = "http://api.census.gov/data/2013/acs5/profile?get="+req.params.indicator+"&for=zip+code+tabulation+area:*"+key;
 
-      // fs.readFile("./public/variables.json",function(e,data)
-      // {
-      //   var read = JSON.parse(data);
-      //
-      // request(url3, function(error, response, body)
-      // {
-      //   if(!error && response.statusCode === 200)
-      //     {
-      //       var data = JSON.parse(body);
-      //       console.log(data.name);
-      //       var obj =
-      //       {
-      //         id: data.name,
-      //         concept: data.concept,
-      //         label: data.label
-      //       }
-      //       read.push(obj);
-      //
-      //       fs.writeFile("./public/variables.json", JSON.stringify(read),function(e)
-      //       {
-      //         console.log("Variable added!");
-      //       })
-      //
-      //       }
-      //   });
-      // });
+    request(url2, function(error, response, body)
+    {
+      if(!error && response.statusCode === 200)
+        {
+          var data = JSON.parse(body);
 
+          data.forEach(function(zip)
+          {
+            var datum =
+            {
+              id: zip[1],
+              rate: parseFloat(zip[0])
+            }
+            array2.push(datum);
+          });
+
+          fs.writeFile("./public/zip_rate.json",JSON.stringify(array2),function(e)
+          {
+            console.log("Zip Code Rate done!");
+          })
+
+          var lowest = Number.POSITIVE_INFINITY;
+          var highest = Number.NEGATIVE_INFINITY;
+          var tmp;
+
+          for(var i=array2.length-1;i>=1;i--)
+            {
+              tmp = array2[i].rate;
+              if(tmp < lowest) lowest=tmp;
+              if(tmp > highest) highest =tmp;
+            }
+            console.log("highest: "+highest,"lowest: "+lowest);
+          }
+      });
     };
-})
+});
 
 
 });
