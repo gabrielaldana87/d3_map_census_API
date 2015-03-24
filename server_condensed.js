f023a87fa56425c0ea8b57f77cc08b65c08d0603
gabrielaldana87 <gabrielaldana87@gmail.com>
2015-03-24 13:12:10 -0400

    changed the port numbers

--- server_condensed.js
+++ server_condensed.js
@@ -19,12 +19,12 @@ fs.readFile("./key.txt",function(e,data)
 {
   var key = data.toString();
 
-app.get('/',function(req,res)
+app.get('/choropleths',function(req,res)
 {
   res.sendFile(__dirname + '/public/us_map_indicators.html');
 });
 
-app.get('/variables', function(req,res)
+app.get('/choropleths/variables', function(req,res)
 {
 db.all("SELECT * FROM variables", function(err, rows)
   {
@@ -33,13 +33,13 @@ db.all("SELECT * FROM variables", function(err, rows)
   });
 });
 
-app.get('/zip', function(req,res)
+app.get('/choropleths/zip', function(req,res)
 {
   res.sendFile(__dirname + '/public/nyc_map.html');
 });
 
 
-app.get('/CREATE/:indicator',function(req, res)
+app.get('choropleths/CREATE/:indicator',function(req, res)
   {
   if(req.params.indicator!=='favicon.ico')
     {
@@ -147,7 +147,7 @@ for(i=0;i<read.length;i++){
 });
 
 
-app.get('/:indicator',function(req, res)
+app.get('choropleths/:indicator',function(req, res)
   {
   if(req.params.indicator!=='favicon.ico')
     {
@@ -158,49 +158,12 @@ app.get('/:indicator',function(req, res)
             if(err) {throw err;}
             res.json(row);
             });
-
-    // var url2 = "http://api.census.gov/data/2013/acs5/profile?get="+req.params.indicator+"&for=zip+code+tabulation+area:*"+key;
-    //
-    // request(url2, function(error, response, body)
-    // {
-    //   if(!error && response.statusCode === 200)
-    //     {
-    //       var data = JSON.parse(body);
-    //
-    //       data.forEach(function(zip)
-    //       {
-    //         var datum =
-    //         {
-    //           id: zip[1],
-    //           rate: parseFloat(zip[0])
-    //         }
-    //         array2.push(datum);
-    //       });
-    //
-    //       fs.writeFile("./public/zip_rate.json",JSON.stringify(array2),function(e)
-    //       {
-    //         console.log("Zip Code Rate done!");
-    //       })
-    //
-    //       var lowest = Number.POSITIVE_INFINITY;
-    //       var highest = Number.NEGATIVE_INFINITY;
-    //       var tmp;
-    //
-    //       for(var i=array2.length-1;i>=1;i--)
-    //         {
-    //           tmp = array2[i].rate;
-    //           if(tmp < lowest) lowest=tmp;
-    //           if(tmp > highest) highest =tmp;
-    //         }
-    //         console.log("highest: "+highest,"lowest: "+lowest);
-    //       }
-    //   });
     };
 });
 
 
 });
-var server = app.listen(3000,function()
+var server = app.listen(80,function()
 {
-  console.log("listening on port 3000")
+  console.log("listening on port 80")
 });
